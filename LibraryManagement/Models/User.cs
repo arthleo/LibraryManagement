4ebb5aa9a5f0ea8f724444cc64468fa7963dc980
kishorebhandari000 <kishorebhandari000@gmail.com>
2026-05-06 12:14:27 +1000

    Created User.cs now add controller

---
+++ LibraryManagement/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Full name is required")]
+        [StringLength(100)]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        // "Admin" or "Member"
+        [Required]
+        public string Role { get; set; } = "Member";
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // Navigation properties
+        public ICollection<BorrowingTransaction> BorrowingTransactions { get; set; } = new List<BorrowingTransaction>();
+        public ICollection<Feedback> Feedbacks { get; set; } = new List<Feedback>();
+    }
+}
