3d750d44146c63d696a6dc5c4a54dd8257e14b11
namuna24 <20032259@students.koi.edu.au>
2026-05-19 09:08:13 +0000

    home page edited

--- LibraryManagement/Controllers/HomeController.cs
+++ LibraryManagement/Controllers/HomeController.cs
@@ -1,25 +1,51 @@
+using LibraryManagement.Data;
 using LibraryManagement.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace LibraryManagement.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        // Constructor
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
+        // Home Page
+        public async Task<IActionResult> Index()
+        {
+            // Get latest 6 books that have uploaded cover images
+            var books = await _context.Books
+                .Where(b => !string.IsNullOrEmpty(b.CoverImagePath))
+                .OrderByDescending(b => b.Id)
+                .Take(6)
+                .ToListAsync();
+
+            return View(books);
+        }
+
+        // Privacy Page
         public IActionResult Privacy()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        // Error Page
+        [ResponseCache(Duration = 0,
+            Location = ResponseCacheLocation.None,
+            NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id
+                    ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
