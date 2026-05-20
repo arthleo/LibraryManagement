94c0287954c872ed48e3c78927dd287a648d0f00
kishorebhandari000 <kishorebhandari000@gmail.com>
2026-05-20 11:22:34 +1000

    Unit testing for book controller done.

---
+++ LibraryManagement.Tests/BooksControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagement.Controllers;
+using LibraryManagement.Data;
+
+namespace LibraryManagement.Tests
+{
+    [TestClass]
+    public class BooksControllerTests
+    {
+        [TestMethod]
+        public async Task Index_ReturnsViewResult()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("TestDatabase")
+                .Options;
+
+            using var context = new ApplicationDbContext(options);
+
+            var controller = new BooksController(context);
+
+            // Act
+            var result = await controller.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+    }
+}
